d0c013bcbe55ba88302ceeda7c33f8ffa1d4a719
kumar7075 <bodapudikumar@gmail.com>
2026-01-06 10:10:09 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'dailymark-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './js/db.js',
+  './js/dashboard.js',
+  './js/tasks.js',
+  './js/money.js',
+  './js/transactions.js',
+  './js/backup.js',
+  './js/notifications.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
